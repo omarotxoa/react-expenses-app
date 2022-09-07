529cb7b4806b20fa190b9fb99ac057b38f52284c
Omar Ochoa <omar@ochoawebdesign.com>
2022-09-06 18:23:45 -0700

    Script dynamic placeholders for ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,12 +2,15 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2022, 8, 20);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 300;
   return (
     <div className='expense-item'>
-      <div>September 5, 2022</div> 
+      <div>{expenseDate.toISOString()}</div> 
       <div className='expense-item__description'>
-        <h2>Car Insurance</h2>
-        <div className='expense-item__price'>$300</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
       </div>
     </div>
   );
